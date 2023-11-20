c034c562ca6a45bd6ae62439a5004eb6bb448179
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2023-11-20 13:23:13 +0100

    add icons

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Nav = () => {
   return (
     <nav>
-        <article className='sm:flex lg:hidden'>
-            <h1>mobile nav</h1>
+        <article className='flex lg:hidden border-b-4 border-main-green px-10 py-4 justify-between'>
+            <Image
+                src="images/initial-profile-picture.svg"
+                width={60}
+                height={60}
+                alt='no profile picture found'
+                className='border-2 border-main-green rounded-full '
+            />
+            <Image
+                src="images/crown-logo.svg"
+                width={60}
+                height={60}
+                alt='crown logo'
+            />
+        </article>
+        <article className='lg:hidden border-t-4 border-main-green'>
+
         </article>
     </nav>
   )
